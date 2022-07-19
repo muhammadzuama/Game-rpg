f535c7135126e946a8317ee130700dbb1cf6fe82
muhammadzuama <108447400+muhammadzuama@users.noreply.github.com>
2022-07-19 23:26:18 +0700

    game rpg use C#
    try to make basic game use inheritance on C#

---
+++ TugasMuhammad Zuama/TugasMuhammad Zuama/Class1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+
+namespace usersystem
+{
+    class Player
+    {
+        internal string username;
+
+        public Player(string _username)
+        {
+            this.username = _username;
+        }
+
+    }
+
+    class Role
+    {
+        internal string nama_skil_1, nama_skill_2;
+        protected int skill1_damage, skill2_damage;
+        internal int Kekuatan, Pertahanan, nyawa;
+    }
+
+    class Assashin : Role
+    {
+        int attack_stack, total_damage;
+        Player user;
+        public Assashin(Player _user)
+        {
+            base.Kekuatan = 40;
+            base.Pertahanan = 60;
+            base.nyawa = 100;
+
+
+            this.user = _user;
+            base.nama_skil_1 = "Pedang Emas";
+            base.skill1_damage = 50;
+            base.nama_skill_2 = "Pedang Petir";
+            base.skill2_damage =80 ;
+        }
+
+        public int Menyerang(int type_attack)
+
+        {
+            if (type_attack == 1)
+            {
+                Console.WriteLine("Normal Attack - Jumlah Damage: " + base.Kekuatan);
+                this.total_damage = base.Kekuatan;
+            }
+            else if (type_attack == 2)
+            {
+                Console.WriteLine(this.nama_skil_1 + " - Jumlah Damage: " + this.nama_skil_1);
+                this.total_damage = this.skill1_damage;
+            }
+            else if (type_attack == 3)
+            {
+                Console.WriteLine(this.nama_skill_2 + " - Jumlah Damage: " + this.nama_skill_2);
+                this.total_damage = this.skill2_damage;
+            }
+
+            if (attack_stack == 10)
+            {
+                int extra_damage = Convert.ToInt32(Math.Ceiling(total_damage * 0.1));
+                Console.WriteLine("Extra Attack - Damage Dealt: " + extra_damage);
+                attack_stack = 0;
+            }
+            this.attack_stack += 1;
+            return this.total_damage;
+        }
+
+        public void defend(int attack_received)
+        {
+            int tmp_attack = base.Pertahanan - attack_received;
+            base.nyawa -= tmp_attack;
+            Console.WriteLine("Keras Damage: " + tmp_attack + " Sisa Nyawa: " + base.nyawa + "\n\n");
+        }
+
+        public bool ALive()
+        {
+            if (base.nyawa < 0)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    class Tank : Role
+    {
+        int attack_stack, total_damage;
+        Player user;
+        public Tank(Player _user)
+        {
+            base.Kekuatan = 30;
+            base.Pertahanan = 70;
+            base.nyawa = 120;
+
+            this.user = _user;
+            base.nama_skil_1 = "Pukulan Topan";
+            base.skill1_damage = 50;
+            base.nama_skill_2 = "Pukulan Petir";
+            base.skill2_damage = 80;
+        }
+
+        public int Menyerang(int type_attack)
+
+        {
+            if (type_attack == 1)
+            {
+                Console.WriteLine("Normal Attack - Jumlah Damage: " + base.Kekuatan);
+                this.total_damage = base.Kekuatan;
+            }
+            else if (type_attack == 2)
+            {
+                Console.WriteLine(this.nama_skil_1 + " - Jumlah Damage: " + this.nama_skil_1);
+                this.total_damage = this.skill1_damage;
+            }
+            else if (type_attack == 3)
+            {
+                Console.WriteLine(this.nama_skill_2 + " - Jumlah Damage: " + this.nama_skill_2);
+                this.total_damage = this.skill2_damage;
+            }
+
+            if (attack_stack == 10)
+            {
+                int extra_damage = Convert.ToInt32(Math.Ceiling(total_damage * 0.1));
+                Console.WriteLine("Extra Attack - Damage Dealt: " + extra_damage);
+                attack_stack = 0;
+            }
+            this.attack_stack += 1;
+            return this.total_damage;
+        }
+        public void defend(int attack_received)
+        {
+            int tmp_attack = base.Pertahanan - attack_received;
+            base.nyawa -= tmp_attack;
+            Console.WriteLine("Keras Damage: " + tmp_attack + " Sisa Nyawa: " + base.nyawa + "\n\n");
+        }
+        public bool ALive()
+        {
+            if (base.nyawa < 0)
+                return false;
+            else
+                return true;
+        }
+    }
+
+
+    class Mage : Role
+    {
+        int attack_stack, total_damage;
+        Player user;
+        public Mage(Player _user)
+        {
+            base.Kekuatan = 40;
+            base.Pertahanan = 60;
+            base.nyawa = 100;
+
+
+            this.user = _user;
+            base.nama_skil_1 = "Sihir Air";
+            base.skill1_damage = 50;
+            base.nama_skill_2 = "Sihir Petir";
+            base.skill2_damage = 70;
+        }
+
+        public int Menyerang(int type_attack)
+
+        {
+            if (type_attack == 1)
+            {
+                Console.WriteLine("Normal Attack - Damage Dealt: " + base.Kekuatan);
+                this.total_damage += base.Kekuatan;
+            }
+            else if (type_attack == 2)
+            {
+                Console.WriteLine(this.nama_skil_1 + " - Damage Dealt: " + this.nama_skil_1);
+                this.total_damage += this.skill1_damage;
+            }
+            else if (type_attack == 3)
+            {
+                Console.WriteLine(this.nama_skill_2 + " - Damage Dealt: " + this.nama_skill_2);
+                this.total_damage += this.skill2_damage;
+            }
+
+            if (attack_stack == 10)
+            {
+                int extra_damage = Convert.ToInt32(Math.Ceiling(total_damage * 0.1));
+                Console.WriteLine("Extra Attack - Damage Dealt: " + extra_damage);
+                attack_stack = 0;
+            }
+            this.attack_stack += 1;
+            return this.total_damage;
+        }
+
+        public void defend(int attack_received)
+        {
+            int tmp_attack = base.Pertahanan - attack_received;
+            base.nyawa -= tmp_attack;
+            Console.WriteLine("keras damage : " + tmp_attack + " sisa nyawa : " + base.nyawa + "\n\n");
+        }
+
+        public bool ALive()
+        {
+            if (base.nyawa < 0)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    class musuh
+    {
+        internal string nama_skil_1, nama_skill_2;
+        protected int skill1_damage, skill2_damage;
+        internal int Kekuatan, Pertahanan, nyawa;
+    }
+    class godzila: musuh
+    {
+        int attack_stack, total_damage;
+        Player user;
+        public godzila(Player _user)
+        {
+            base.Kekuatan = 40;
+            base.Pertahanan = 60;
+            base.nyawa = 100;
+
+
+            base.nama_skil_1 = "Pedang Emas";
+            base.skill1_damage = 50;
+            base.nama_skill_2 = "Pedang Petir";
+            base.skill2_damage = 80;
+        }
+    }
+
+
+
+}
+
---
+++ TugasMuhammad Zuama/TugasMuhammad Zuama/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using usersystem;
+
+class gameplay
+{
+    public static void Main(string[] args)
+    {
+        Random Godzila_attack = new Random();
+        int Godzila = 150;
+        Console.Write("Username = ");
+        string username = Console.ReadLine();
+        Player p1 = new Player(username);
+        Console.WriteLine("[1] ASSASSIN |Kekuatan = 40 |Pertahanan = 60|nyawa = 100 |");
+        Console.WriteLine("[2] TANK     |Kekuatan = 30 |Pertahanan = 70|nyawa = 120 |");
+        Console.WriteLine("[3] MAGE     |Kekuatan = 90 |Pertahanan = 20|nyawa = 80  |");
+        Console.Write("Pilih Role = ");
+        int Pilih = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+        if (Pilih == 1)
+        {
+            Assashin p1_role = new Assashin(p1);
+            while (true)
+            {
+                Console.WriteLine("Serang Monster Itu !!!!!!");
+                Console.WriteLine("[1].Serangan Normal [2]." + p1_role.nama_skil_1 + " [3]." + p1_role.nama_skill_2 + " : ");
+                int attack_type = Convert.ToInt32(Console.ReadLine());
+                p1_role.defend(Godzila_attack.Next(1, 50));
+                Godzila -= p1_role.Menyerang(attack_type);
+                Console.WriteLine("Sisa darah musuh = " + Godzila);
+                if (Godzila <= 0)
+                {
+                    Console.WriteLine(" YOU WINN !!!!");
+                    System.Environment.Exit(1);
+                }
+                if (!p1_role.ALive())
+                {
+                    Console.WriteLine("GAME OVER!!!");
+                    System.Environment.Exit(1);
+                }
+            }
+        }
+        else if (Pilih == 2)
+        {
+            Tank p1_role = new Tank(p1);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("[1].Serangan Normal [2]." + p1_role.nama_skil_1 + " [3]." + p1_role.nama_skill_2 + " : ");
+                int attack_type = Convert.ToInt32(Console.ReadLine());
+                p1_role.Menyerang(attack_type);
+                p1_role.defend(Godzila_attack.Next(1, 40));
+                Godzila -= p1_role.Menyerang(attack_type);
+                Console.WriteLine("Sisa darah musuh = " + Godzila);
+                if (Godzila <= 0)
+                {
+                    Console.WriteLine(" YOU WINN !!!!");
+                    System.Environment.Exit(1);
+                }
+
+                if (!p1_role.ALive())
+                {
+                    System.Environment.Exit(1);
+                    Console.WriteLine("GAME OVER!!!");
+                }
+            }
+        }
+        else if (Pilih == 3)
+        {
+            Mage p1_role = new Mage(p1);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("[1].Serangan Normal [2]." + p1_role.nama_skil_1 + " [3]." + p1_role.nama_skill_2 + " : ");
+                int attack_type = Convert.ToInt32(Console.ReadLine());
+                p1_role.Menyerang(attack_type);
+                p1_role.defend(Godzila_attack.Next(1, 15));
+                Godzila -= p1_role.Menyerang(attack_type);
+                Console.WriteLine("Sisa darah musuh = " + Godzila);
+                if (Godzila <= 0)
+                {
+                    Console.WriteLine(" YOU WINN !!!!");
+                    System.Environment.Exit(1);
+                }
+                if (!p1_role.ALive())
+                {
+                    Console.WriteLine("GAME OVER!!!");
+                    System.Environment.Exit(1);
+                }
+            }
+        }
+    }
+}
